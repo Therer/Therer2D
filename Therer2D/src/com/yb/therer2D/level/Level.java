b344b026730f6f3b1b1d0ea8ab83f872bff8166a
yannick Baekelmans <yannick.baekelmans@hotmail.com>
2016-07-31 18:09:53 +0200

    Episode 31 - Rendering Corner Pins

--- Therer2D/src/com/yb/therer2D/level/Level.java
+++ Therer2D/src/com/yb/therer2D/level/Level.java
@@ -30,6 +30,10 @@ public class Level {
 	private void time() {
 	}
 	
-	public void render(int xScroll, int yScroll, Screen screen) {		
+	public void render(int xScroll, int yScroll, Screen screen) {				
+		int x0 = xScroll >> 4;
+		int x1 = (xScroll + screen.width + 16) >> 4;
+		int y0 = yScroll >> 4;
+		int y1 = (yScroll + screen.height + 16) >> 4;
 	}
 }
