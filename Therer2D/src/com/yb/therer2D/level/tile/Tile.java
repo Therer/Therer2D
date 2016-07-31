1eaede233ac6fc7957ddeec928b9da63568b3c2f
yannick Baekelmans <yannick.baekelmans@hotmail.com>
2016-07-31 17:19:07 +0200

    Episode 26 - The Tile Class

---
+++ Therer2D/src/com/yb/therer2D/level/tile/Tile.java
@@ -0,0 +1,21 @@
+package com.yb.therer2D.level.tile;
+
+import com.yb.therer2D.graphics.Screen;
+import com.yb.therer2D.graphics.Sprite;
+
+public class Tile {
+
+	public int x, y;
+	public Sprite sprite;
+	
+	public Tile(Sprite sprite) {
+		this.sprite = sprite;
+	}
+	
+	public void render(int x, int y, Screen screen) {	
+	}
+	
+	public boolean solid() {
+		return false;
+	}
+}
