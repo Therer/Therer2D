c13cd0a2dc2febecffda1cb28b51f12ed39eb7af
yannick Baekelmans <yannick.baekelmans@hotmail.com>
2016-07-31 16:54:15 +0200

    Episode 20 - Sprites

---
+++ Therer2D/src/com/yb/therer2D/graphics/Sprite.java
@@ -0,0 +1,25 @@
+package com.yb.therer2D.graphics;
+
+public class Sprite {
+	
+	public final int SIZE;
+	private int x, y;
+	public int[] pixels;
+	private SpriteSheet sheet;
+	
+	public Sprite(int size, int x, int y, SpriteSheet sheet) {
+		SIZE = size;
+		pixels = new int[SIZE * SIZE];
+		this.x = x * SIZE;
+		this.y = y * SIZE;
+		this.sheet = sheet;
+		load();
+	}
+	private void load() {
+		for(int y = 0; y < SIZE; y++) {
+			for(int x = 0; x < SIZE; x++) {
+				pixels[x + y * SIZE] = sheet.pixels[(x + this.x) + (y + this.y) * sheet.SIZE];
+			}
+		}
+	}
+}
