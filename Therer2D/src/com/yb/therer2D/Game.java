a75e3807bd1f5a467a9259ed511631a01bbf7346
yannick Baekelmans <yannick.baekelmans@hotmail.com>
2016-07-31 15:19:20 +0200

    Episode 5 - Buffer Strategy

--- Therer2D/src/com/yb/therer2D/Game.java
+++ Therer2D/src/com/yb/therer2D/Game.java
@@ -2,6 +2,7 @@ package com.yb.therer2D;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -39,8 +40,20 @@ public class Game extends Canvas implements Runnable{
 	
 	public void run() {
 		while(running){
-			System.out.println("Running...");
+			update();
+			render();
 		}
+	}
+	
+	public void update() {
+	}
+	
+	public void render() {
+	    BufferStrategy bs = getBufferStrategy();
+	    if (bs == null) {
+	        createBufferStrategy(3);
+	        return;
+	    }
 	}
 	
 	public static void main(String[] args) {
