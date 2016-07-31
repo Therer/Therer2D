edce39ebdf6bd97d52bf0cc48b2be4f40c922438
yannick Baekelmans <yannick.baekelmans@hotmail.com>
2016-07-31 18:25:16 +0200

    Episode 33 - The Void Tile

---
+++ Therer2D/src/com/yb/therer2D/level/tile/VoidTile.java
@@ -0,0 +1,15 @@
+package com.yb.therer2D.level.tile;
+
+import com.yb.therer2D.graphics.Screen;
+import com.yb.therer2D.graphics.Sprite;
+
+public class VoidTile extends Tile {
+	
+	public VoidTile(Sprite sprite) {
+		super(sprite);
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x ,y , this);
+	}
+}
