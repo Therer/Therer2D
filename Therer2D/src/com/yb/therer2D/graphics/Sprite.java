1cf44b2e2a10d5dd8fb7224cfc372f0f54808b76
yannick Baekelmans <yannick.baekelmans@hotmail.com>
2016-07-31 17:02:00 +0200

    Episode 22 - Drawing Sprites

--- Therer2D/src/com/yb/therer2D/graphics/Sprite.java
+++ Therer2D/src/com/yb/therer2D/graphics/Sprite.java
@@ -7,7 +7,7 @@ public class Sprite {
 	public int[] pixels;
 	private SpriteSheet sheet;
 	
-	public static Sprite grass = new Sprite(16, 0, 0, null);
+	public static Sprite grass = new Sprite(16, 0, 0, SpriteSheet.tiles);
 	
 	public Sprite(int size, int x, int y, SpriteSheet sheet) {
 		SIZE = size;
