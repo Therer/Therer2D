e6a3f9e6a6d4859b3861e067411482bd15c542f7
yannick Baekelmans <yannick.baekelmans@hotmail.com>
2016-08-06 01:00:50 +0200

    Episode 45 - Player Graphics

--- Therer2D/src/com/yb/therer2D/entity/mob/Player.java
+++ Therer2D/src/com/yb/therer2D/entity/mob/Player.java
@@ -1,5 +1,7 @@
 package com.yb.therer2D.entity.mob;
 
+import com.yb.therer2D.graphics.Screen;
+import com.yb.therer2D.graphics.Sprite;
 import com.yb.therer2D.input.Keyboard;
 
 public class Player extends Mob {
@@ -29,7 +31,8 @@ public class Player extends Mob {
 		}
 	}
 	
-	public void render() {		
+	public void render(Screen screen) {		
+		screen.renderPlayer(x, y, Sprite.player0);
 	}
 }
 
