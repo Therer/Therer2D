012c5e3845927cb6754e201f989c525c2e6936e6
yannick Baekelmans <yannick.baekelmans@hotmail.com>
2016-08-06 00:31:06 +0200

    Episode 39 - Entity Framework

---
+++ Therer2D/src/com/yb/therer2D/entity/Entity.java
@@ -0,0 +1,31 @@
+package com.yb.therer2D.entity;
+
+import java.util.Random;
+
+import com.yb.therer2D.graphics.Screen;
+import com.yb.therer2D.level.Level;
+
+public abstract class Entity {
+	
+	public int x, y;
+	private boolean removed = false;
+	protected Level level;
+	protected final Random random = new Random();
+	
+	public void update() {
+		
+	}
+	
+	public void render(Screen screen) {
+		
+	}
+	
+	public void remove() {
+		// Remove from level
+		removed = true;
+	}
+	
+	public boolean isRemoved() {
+		return removed;
+	}
+}
