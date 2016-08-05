f9fb7efd3eacd6fc7fda6e4f9ea2a5407d2d09b1
yannick Baekelmans <yannick.baekelmans@hotmail.com>
2016-08-06 01:04:55 +0200

    Episode 46 - Finishing Player Graphics

--- Therer2D/src/com/yb/therer2D/entity/mob/Player.java
+++ Therer2D/src/com/yb/therer2D/entity/mob/Player.java
@@ -5,34 +5,42 @@ import com.yb.therer2D.graphics.Sprite;
 import com.yb.therer2D.input.Keyboard;
 
 public class Player extends Mob {
-	
+
 	private Keyboard input;
-	
+
 	public Player(Keyboard input) {
 		this.input = input;
 	}
-	
-	public Player(int x,int y, Keyboard input) {
+
+	public Player(int x, int y, Keyboard input) {
 		this.x = x;
 		this.y = y;
 		this.input = input;
 	}
-	
+
 	public void update() {
-		int xa = 0, ya = 0;	
-		
-		if(input.up) ya--;
-		if(input.down) ya++;
-		if(input.left) xa--;
-		if(input.right) xa++;
-		
-		if(xa != 0 || ya != 0) {
+		int xa = 0, ya = 0;
+
+		if (input.up)
+			ya--;
+		if (input.down)
+			ya++;
+		if (input.left)
+			xa--;
+		if (input.right)
+			xa++;
+
+		if (xa != 0 || ya != 0) {
 			move(xa, ya);
 		}
 	}
-	
-	public void render(Screen screen) {		
-		screen.renderPlayer(x, y, Sprite.player0);
+
+	public void render(Screen screen) {
+		int xx = x - 16;
+		int yy = y - 16;
+		screen.renderPlayer(xx, yy, Sprite.player0);
+		screen.renderPlayer(xx + 16, yy, Sprite.player1);
+		screen.renderPlayer(xx, yy + 16, Sprite.player2);
+		screen.renderPlayer(xx + 16, yy + 16, Sprite.player3);
 	}
 }
-
