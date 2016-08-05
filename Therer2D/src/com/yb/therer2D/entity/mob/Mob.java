2c95d4834cd72c1eefede8ff1a18474b6ac906fb
yannick Baekelmans <yannick.baekelmans@hotmail.com>
2016-08-06 00:39:20 +0200

    Episode 42 - The Move Method

--- Therer2D/src/com/yb/therer2D/entity/mob/Mob.java
+++ Therer2D/src/com/yb/therer2D/entity/mob/Mob.java
@@ -10,6 +10,16 @@ public abstract class Mob extends Entity {
 	protected boolean moving = false;
 
 	public void move(int xa, int ya) {
+		if(xa > 0) dir = 1;
+		if(xa < 0) dir = 3;
+		if(ya > 0) dir = 2;
+		if(ya < 0) dir = 0;
+		
+		if(!collision())
+		{
+			x += xa;
+			y += ya;
+		}
 	}
 	
 	public void update() {
