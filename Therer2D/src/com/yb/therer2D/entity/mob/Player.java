3d417d2e0d17244254741e1ba1e2a21994fa053e
yannick Baekelmans <yannick.baekelmans@hotmail.com>
2016-08-06 18:20:04 +0200

    Episode 48 - Player Direction Animation

--- Therer2D/src/com/yb/therer2D/entity/mob/Player.java
+++ Therer2D/src/com/yb/therer2D/entity/mob/Player.java
@@ -7,9 +7,11 @@ import com.yb.therer2D.input.Keyboard;
 public class Player extends Mob {
 
 	private Keyboard input;
+	private Sprite sprite;
 
 	public Player(Keyboard input) {
 		this.input = input;
+		sprite = Sprite.player_forward;
 	}
 
 	public Player(int x, int y, Keyboard input) {
@@ -36,6 +38,10 @@ public class Player extends Mob {
 	}
 
 	public void render(Screen screen) {
-		screen.renderPlayer(x - 16, y - 16, Sprite.player);
+		if(dir == 0) sprite = Sprite.player_forward;
+		if(dir == 1) sprite = Sprite.player_right;
+		if(dir == 2) sprite = Sprite.player_back;
+		if(dir == 3) sprite = Sprite.player_left;
+		screen.renderPlayer(x - 16, y - 16, sprite);
 	}
 }
