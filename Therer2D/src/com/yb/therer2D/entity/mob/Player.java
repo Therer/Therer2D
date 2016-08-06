31ae186f8944af6cfaa338043c567abe1c4acb63
yannick Baekelmans <yannick.baekelmans@hotmail.com>
2016-08-06 18:37:38 +0200

    Episode 50 - Animating Sprites

--- Therer2D/src/com/yb/therer2D/entity/mob/Player.java
+++ Therer2D/src/com/yb/therer2D/entity/mob/Player.java
@@ -8,6 +8,8 @@ public class Player extends Mob {
 
 	private Keyboard input;
 	private Sprite sprite;
+	private int anim = 0;
+	private boolean walking = false;
 
 	public Player(Keyboard input) {
 		this.input = input;
@@ -23,6 +25,11 @@ public class Player extends Mob {
 	public void update() {
 		int xa = 0, ya = 0;
 
+		if (anim < 7500)
+			anim++;
+		else
+			anim = 0;
+
 		if (input.up)
 			ya--;
 		if (input.down)
@@ -34,14 +41,58 @@ public class Player extends Mob {
 
 		if (xa != 0 || ya != 0) {
 			move(xa, ya);
-		}
+			walking = true;
+		} else
+			walking = false;
 	}
 
 	public void render(Screen screen) {
-		if(dir == 0) sprite = Sprite.player_forward;
-		if(dir == 1) sprite = Sprite.player_right;
-		if(dir == 2) sprite = Sprite.player_back;
-		if(dir == 3) sprite = Sprite.player_left;
-		screen.renderPlayer(x - 16, y - 16, sprite);
+		int flip = 0;
+
+		if (dir == 0) {
+			sprite = Sprite.player_forward;
+			if (walking) {
+				if (anim % 20 > 10) {
+					sprite = Sprite.player_forward_1;
+				} else {
+					sprite = Sprite.player_forward_2;
+				}
+			}
+		}
+
+		if (dir == 1) {
+			sprite = Sprite.player_side;
+			if (walking) {
+				if (anim % 20 > 10) {
+					sprite = Sprite.player_side_1;
+				} else {
+					sprite = Sprite.player_side_2;
+				}
+			}
+		}
+
+		if (dir == 2) {
+			sprite = Sprite.player_back;
+			if (walking) {
+				if (anim % 20 > 10) {
+					sprite = Sprite.player_back_1;
+				} else {
+					sprite = Sprite.player_back_2;
+				}
+			}
+		}
+
+		if (dir == 3) {
+			sprite = Sprite.player_side;
+			if (walking) {
+				if (anim % 20 > 10) {
+					sprite = Sprite.player_side_1;
+				} else {
+					sprite = Sprite.player_side_2;
+				}
+			}
+			flip = 1;
+		}
+		screen.renderPlayer(x - 16, y - 16, sprite, flip);
 	}
 }
