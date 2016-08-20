ac5490c859839b7e5f794018e8f6732cc590d541
yannick Baekelmans <yannick.baekelmans@hotmail.com>
2016-08-20 16:06:54 +0200

    Episode 76: The Particle Class

---
+++ Therer2D/src/com/yb/therer2D/entity/particle/Particle.java
@@ -0,0 +1,40 @@
+package com.yb.therer2D.entity.particle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.yb.therer2D.entity.Entity;
+import com.yb.therer2D.graphics.Screen;
+import com.yb.therer2D.graphics.Sprite;
+
+public class Particle extends Entity {
+
+	private List<Particle> particles = new ArrayList<Particle>();
+	private Sprite sprite;
+
+	private int life;
+
+	public Particle(int x, int y, int life) {
+		this.x = x;
+		this.y = y;
+		this.life = life;
+		sprite = Sprite.particle_normal;
+		particles.add(this);
+
+	}
+
+	public Particle(int x, int y, int life, int amount) {
+		this(x, y, life);
+		for (int i = 0; i < amount - 1; i++) {
+			particles.add(new Particle(x, y, life));
+		}
+
+	}
+
+	public void update() {
+	}
+
+	public void render(Screen screen) {
+	}
+
+}
